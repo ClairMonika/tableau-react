7cdf02a08d748b265900c585260eaaa54e8afec0
Monika Patel <monika.patel@clairvoyantsoft.com>
2022-09-15 13:40:02 +0530

    added build for deployment

---
+++ precache-manifest.6bcf2536730972c4e30cb24946bfaa0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "89df348c7c7aaf5995e8",
+    "url": "/static/css/main.a465c087.chunk.css"
+  },
+  {
+    "revision": "89df348c7c7aaf5995e8",
+    "url": "/static/js/main.89df348c.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "158e87840a9b5585c0aa",
+    "url": "/static/js/2.158e8784.chunk.js"
+  },
+  {
+    "revision": "f5a069d24c7855d24188c14f570b6a57",
+    "url": "/static/media/github.f5a069d2.svg"
+  },
+  {
+    "revision": "c0125f54f9441aa740384f9f816fcf3b",
+    "url": "/index.html"
+  }
+];
