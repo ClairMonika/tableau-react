a7dac3dcc5bfceaa5d460035273cfbca1f02a0f2
Monika Patel <monika.patel@clairvoyantsoft.com>
2022-09-15 15:23:57 +0530

    remove un necessory code

--- precache-manifest.916d776c4502ea75fb4b7021688cf04d.js
+++ precache-manifest.916d776c4502ea75fb4b7021688cf04d.js
@@ -1,26 +1,26 @@
 self.__precacheManifest = [
   {
-    "revision": "89df348c7c7aaf5995e8",
+    "revision": "33c1bed947420e023c40",
     "url": "/static/css/main.a465c087.chunk.css"
   },
   {
-    "revision": "89df348c7c7aaf5995e8",
-    "url": "/static/js/main.89df348c.chunk.js"
+    "revision": "33c1bed947420e023c40",
+    "url": "/static/js/main.33c1bed9.chunk.js"
   },
   {
     "revision": "fdfcfda2d9b1bf31db52",
     "url": "/static/js/runtime~main.fdfcfda2.js"
   },
   {
-    "revision": "158e87840a9b5585c0aa",
-    "url": "/static/js/2.158e8784.chunk.js"
+    "revision": "377e0f3d6acf10ccfd4d",
+    "url": "/static/js/2.377e0f3d.chunk.js"
   },
   {
     "revision": "f5a069d24c7855d24188c14f570b6a57",
     "url": "/static/media/github.f5a069d2.svg"
   },
   {
-    "revision": "c0125f54f9441aa740384f9f816fcf3b",
+    "revision": "fe0ce6f9312f98c79f62009521887e3b",
     "url": "/index.html"
   }
 ];
