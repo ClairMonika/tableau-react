6ac0ad0ed4b851110143cccb8816625b7e93a2e7
Monika Patel <monika.patel@clairvoyantsoft.com>
2022-10-14 11:53:22 +0530

    new build for the dashboard look and feel

--- precache-manifest.e3ebff31c85553db1b12911f7f471c8b.js
+++ precache-manifest.e3ebff31c85553db1b12911f7f471c8b.js
@@ -1,26 +1,26 @@
 self.__precacheManifest = [
   {
-    "revision": "7c0b47b8c06c8a72016b",
+    "revision": "3244dbbe4cbe75414e1f",
     "url": "/static/css/main.a465c087.chunk.css"
   },
   {
-    "revision": "7c0b47b8c06c8a72016b",
-    "url": "/static/js/main.7c0b47b8.chunk.js"
+    "revision": "3244dbbe4cbe75414e1f",
+    "url": "/static/js/main.3244dbbe.chunk.js"
   },
   {
     "revision": "fdfcfda2d9b1bf31db52",
     "url": "/static/js/runtime~main.fdfcfda2.js"
   },
   {
-    "revision": "377e0f3d6acf10ccfd4d",
-    "url": "/static/js/2.377e0f3d.chunk.js"
+    "revision": "54b5e61bcab7f26e496c",
+    "url": "/static/js/2.54b5e61b.chunk.js"
   },
   {
     "revision": "f5a069d24c7855d24188c14f570b6a57",
     "url": "/static/media/github.f5a069d2.svg"
   },
   {
-    "revision": "17f80999061f7a41bc47d710bd00cb90",
+    "revision": "49a397c0c22c80d049ab287120a6ddfb",
     "url": "/index.html"
   }
 ];
